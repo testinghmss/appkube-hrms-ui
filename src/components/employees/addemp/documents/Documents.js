7819228de9f9adefe0bbaa008aa71d410f460825
syedsmuzakkir <syedsmuzakkir46@gmail.com>
2024-03-14 09:37:44 +0530

    Functionality for Uploading images added with api integration displaying data

--- src/components/employees/addemp/documents/Documents.js
+++ src/components/employees/addemp/documents/Documents.js
@@ -1,21 +1,145 @@
 // "use server"
 "use client"
 import React from 'react'
-import Upload from './upload'
+// import Upload from './upload'
 import { FaRegFileAlt } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
 import {Progress, Table} from "antd"
 // import { AiFillDelete } from "react-icons/ai";
 // import Delete from './Table';
 // import FileTable from './FileTable';
+import axios from 'axios';
 import FileTable from './FileTable';
+import { useState } from 'react';
+
+import { Upload } from "antd";
+import Image from 'next/image';
+import { InboxOutlined } from '@ant-design/icons';
+const { Dragger } = Upload; 
+
+
 
 
 
 const Documents = ({tab,setTab}) => {
 
+  
+
+
+
+
+
+
+
+
+
+  
+  
+const [req, setReq] = useState(
+  {fileName:'' , data: '' }
+);
+
+const [fileuploaded, setfileuploaded] = useState(false)
+
+const handleFileChange = (info) => {
+  const file = info.file.originFileObj; // Access the selected file object
+  console.log("THis is file",file)
+  console.log("This is info file",info.file)
+console.log(info.file, info.fileList, 'these are lists of files ');
+console.log(info.fileList,'THis is inof multiple ')
+
+  if (file){
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      setReq({ fileName: file.name, data: base64 });
+      setfileuploaded(true)
+    
+
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
+console.log(req)
+
+
+
+  const [Attachments, setAttachments] = useState([])
+
+
+const uploadFile = async () => {
+
+
+  try {
+
+const response = await axios.post( 'https://i3mdnxvgrf.execute-api.us-east-1.amazonaws.com/dev/docUpload' ,
+
+      req,
+
+    );
+
+    console.log(response.data);
+    alert('Image uploaded successfully!');
+    // setAttachments(response.data.link);
+    setAttachments([...Attachments, response.data.link])
+
+  } 
+
+catch (error) {
+    console.error(error);
+    console.log(error)
+    alert('Error uploading image. Please try again.');
+  }
+
+
+}
+
+
+if(fileuploaded){
+  // useEffect(()=>{
+    uploadFile(),
+    setfileuploaded(false)
+    
+    }
+console.log(Attachments)
+  
+  const Upload =()=>{
   return (
-    <div className="w-full h-full p-10 flex flex-col ">
+  // <Dragger {...props}>
+  //   <p className="ant-upload-drag-icon ">
+  //     <InboxOutlined />
+  //   </p>
+  //   <p className="ant-upload-text">Click or drag file to this area to upload</p>
+  //   <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+  // </Dragger>
+  <>
+  <Dragger multiple onChange={(e)=>{handleFileChange(e)}}  >
+  <p className="ant-upload-drag-icon">
+    <InboxOutlined />
+  </p>
+  <p className="ant-upload-text">Click or drag file to this area to upload</p>
+  <p className="ant-upload-hint">
+    Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+    banned files.
+  </p>
+</Dragger>
+<div  >
+  <h2>Uploaded Files </h2>
+  <div className='flex flex-row gap-4 p-4'> 
+
+  
+  {Attachments.map((e, index)=>{return  <Image key={index} src={e} alt="Uploaded images" height={50} width={50}/>})}
+  </div>
+  </div>
+
+</>
+);
+
+  }
+
+  return (
+    <div className="w-full h-full p-10 flex flex-col " >
       <div>
         <Upload />
       </div>
--- src/components/employees/addemp/documents/upload.jsx
+++ src/components/employees/addemp/documents/upload.jsx
@@ -1,34 +1,118 @@
 "use client"
 import React from 'react';
+import { useState } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
-const { Dragger } = Upload;
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
-};
-const UploadFile = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon ">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">Support for a single or bulk upload.</p>
-  </Dragger>
+import axios from 'axios';
+import Image from 'next/image';
+
+import { Upload } from "antd";
+
+import { InboxOutlined } from '@ant-design/icons';
+const { Dragger } = Upload; 
+
+
+
+
+
+
+
+
+const UploadFile = () =>{ 
+  
+  
+const [req, setReq] = useState(
+  {fileName:'' , data: '' }
 );
+
+const [fileuploaded, setfileuploaded] = useState(false)
+
+const handleFileChange = (info) => {
+  const file = info.file.originFileObj; // Access the selected file object
+  console.log("THis is file",file)
+  console.log("This is info file",info.file)
+console.log(info.file, info.fileList, 'these are lists of files ');
+console.log(info.fileList,'THis is inof multiple ')
+
+  if (file){
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      setReq({ fileName: file.name, data: base64 });
+      setfileuploaded(true)
+    
+
+    };
+    reader.readAsDataURL(file);
+  }
+};
+
+console.log(req)
+
+
+
+  const [Attachments, setAttachments] = useState([])
+
+
+const uploadFile = async () => {
+
+
+  try {
+
+const response = await axios.post( 'https://i3mdnxvgrf.execute-api.us-east-1.amazonaws.com/dev/docUpload' ,
+
+      req,
+
+    );
+
+    console.log(response.data);
+    alert('Image uploaded successfully!');
+    // setAttachments(response.data.link);
+    setAttachments([...Attachments, response.data.link])
+
+  } 
+
+catch (error) {
+    console.error(error);
+    console.log(error)
+    alert('Error uploading image. Please try again.');
+  }
+
+
+}
+
+
+if(fileuploaded){
+  // useEffect(()=>{
+    uploadFile(),
+    setfileuploaded(false)
+    
+    }
+console.log(Attachments)
+  
+  
+  return (
+  // <Dragger {...props}>
+  //   <p className="ant-upload-drag-icon ">
+  //     <InboxOutlined />
+  //   </p>
+  //   <p className="ant-upload-text">Click or drag file to this area to upload</p>
+  //   <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+  // </Dragger>
+  <>
+  <Dragger multiple onChange={(e)=>{handleFileChange(e)}}  >
+  <p className="ant-upload-drag-icon">
+    <InboxOutlined />
+  </p>
+  <p className="ant-upload-text">Click or drag file to this area to upload</p>
+  <p className="ant-upload-hint">
+    Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+    banned files.
+  </p>
+</Dragger>
+<div>{Attachments.map((e)=>{return  <Image src={e} alt="Uploaded images" height={50} width={50}/>})}</div>
+
+</>
+);
+
+}
 export default UploadFile;
