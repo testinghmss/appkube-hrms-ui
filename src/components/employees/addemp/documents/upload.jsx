9062de8cf49bf327f7c057455e89c7121d3ed69a
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2024-03-25 09:33:37 +0530

    pullng code from github

--- src/components/employees/addemp/documents/upload.jsx
+++ src/components/employees/addemp/documents/upload.jsx
@@ -24,7 +24,7 @@ const props = {
 };
 const UploadFile = () => (
   <Dragger {...props}>
-    <p className="ant-upload-drag-icon ">
+    <p className="ant-upload-drag-icon">
       <InboxOutlined />
     </p>
     <p className="ant-upload-text">Click or drag file to this area to upload</p>
