1de48f4790ce56c1460773b273da756246d98726
shaistha-samreen <shaisthasamreen786@gmail.com>
2024-04-16 10:09:04 +0530

    Update UI and worked on the tracker page

--- src/app/hrms/loading.js
+++ src/app/hrms/loading.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { LockOutlined, UserOutlined, LoadingOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input, Spin } from "antd";
 
-
 const loading = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-[#ffffffa1] z-10">
